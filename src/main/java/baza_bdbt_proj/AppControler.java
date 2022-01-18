a40cafc5aed50c3f4891cb0082cb0e73e6eaad4f
Jakub Marcin Kozowski <01162784@pw.edu.pl>
2022-01-18 16:16:46 +0100

    kuba

--- src/main/java/baza_bdbt_proj/AppControler.java
+++ src/main/java/baza_bdbt_proj/AppControler.java
@@ -2,13 +2,15 @@ package baza_bdbt_proj;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 @Controller
-public class AppControler {
+public class AppControler  implements WebMvcConfigurer {
     @Autowired
     private ModeleDAO dao;
     @RequestMapping("/")
@@ -18,4 +20,15 @@ public class AppControler {
         return "index";
 
     }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/log").setViewName("log");
+        registry.addViewController("/").setViewName("log");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+
+
+
 }
